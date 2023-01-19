bdf68b634dbfc67c1e9c87959be5ac5e24897cec
dratsavong <dratsavong@ymail.com>
2023-01-19 12:49:13 -0500

    adding id field to typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -2,12 +2,14 @@ const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
   type User {
+    _id: ID
     username: String
     email: String
     password: String
   }
 
   type Dog {
+    _id: ID
     name: String
     age: Int
     breed: String
